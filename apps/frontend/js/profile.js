486c16a9002584cb009821ceea8b8a9d0bc4309f
chisomdaniel <chinwezechisom@gmail.com>
2026-03-13 19:56:20 +0100

    feat: add profile data and fetch functionality for profile page

---
+++ apps/frontend/js/profile.js
@@ -0,0 +1,15 @@
+async function fetchProfile(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+  }
+}
+
+fetchProfile("../assets/data/contributors.json");
