04ebd64f3848715e14cb4cab7a99762b2c35959c
Michael Rukevwe Omonedo <105046150+Rukkyoo@users.noreply.github.com>
2026-03-17 17:46:26 +0100

    Chore: Remove console log from getActivePage function
    Removed console log of the current pathname.

--- apps/frontend/js/sidebar.js
+++ apps/frontend/js/sidebar.js
@@ -1,6 +1,5 @@
 function getActivePage() {
   const currentPage = window.location.pathname.split("/").pop();
-  console.log(window.location.pathname);
   const links = document.querySelectorAll(".sidebar-btn");
   links.forEach((link) => {
     if (link.getAttribute("href") === currentPage) {
