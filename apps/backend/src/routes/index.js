22de64bc2c570489b01c847f5118440ddb0cf5a3
vaden89 <isaacshosanya89@gmail.com>
2026-03-19 22:15:17 +0100

    feat: worked on authentication for admin through supabase auth

--- apps/backend/src/routes/index.js
+++ apps/backend/src/routes/index.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import authRoutes from "./auth.route.js";
+import adminRoutes from "./admin.route.js";
+import uploadRoutes from "./upload.route.js";
 import nominationRoutes from "./nomination.routes.js";
+import { authenticate } from "../middleware/auth.js";
 
 const router = Router();
 
+router.use("/auth", authRoutes);
+router.use("/upload", uploadRoutes);
 router.use("/nomination", nominationRoutes);
+router.use("/admin", authenticate, adminRoutes);
 
 export default router;
